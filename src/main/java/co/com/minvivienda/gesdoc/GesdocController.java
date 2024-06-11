7775c40b542354a78c64f1a65e9f727eef4372ce
James Romero <jromero@viventic.com>
2024-06-11 17:50:43 -0500

    .

--- src/main/java/co/com/minvivienda/gesdoc/GesdocController.java
+++ src/main/java/co/com/minvivienda/gesdoc/GesdocController.java
@@ -62,6 +62,63 @@ public class GesdocController {
 	        		+ "    \"actionDate\": \"20240611063034-0500\""
 	        		+ "}";
 	        
+	        HttpEntity<String> jsonEntity = new HttpEntity<String>(jsonResponse, jsonResponseHeaders);
+	        HttpHeaders jsonFileHeaders = new HttpHeaders();
+	        jsonFileHeaders.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+	        jsonFileHeaders.add(HttpHeaders.TRANSFER_ENCODING, "binary");
+	        jsonFileHeaders.set("Content-ID", "<etiqueta.png>");
+	        HttpEntity<ByteArrayResource> fileEntity = new HttpEntity<ByteArrayResource>(resource, jsonFileHeaders);
+	        
+	        HttpHeaders headers = new HttpHeaders();
+	        headers.setContentType(MediaType.MULTIPART_MIXED);
+	        
+	        MultiValueMap<String, Object> responseBody = new LinkedMultiValueMap<>();
+	        responseBody.clear();
+	        responseBody.set("root", jsonEntity);
+	        responseBody.set("etiqueta.png", fileEntity);
+	        
+	        return new ResponseEntity<MultiValueMap<String, Object>>(responseBody, headers, HttpStatus.OK);
+    	}catch(Exception ex) {
+    		ex.printStackTrace();
+    		return new ResponseEntity<String>("EXCEPCION ", HttpStatus.INTERNAL_SERVER_ERROR);
+    	}
+    }
+    
+    
+    @PostMapping(value = "/mock/createInner", produces = {"multipart/mixed"}, consumes = {"multipart/form-data"})
+    public ResponseEntity<?> createInner(@RequestParam Map<String, MultipartFile> files)  throws Exception {
+    	
+    	try {
+	    	String fileName = "etiqueta.png";
+	        byte[] etiquetaBytes = readImageFromClasspath(fileName);
+	        System.out.println("etiquetaBytes = " + etiquetaBytes.length);
+	    	if (etiquetaBytes != null && etiquetaBytes.length <= 0) {
+	        	System.out.println("Archivo no existe");
+	            return ResponseEntity.notFound().build();
+	        }
+	        
+	        ByteArrayResource resource = new ByteArrayResource(etiquetaBytes) {
+	            @Override
+	            public String getFilename() {
+	                return fileName;
+	            }
+	        };
+	        
+	        HttpHeaders jsonResponseHeaders = new HttpHeaders();
+	        jsonResponseHeaders.setContentType(MediaType.APPLICATION_JSON_UTF8);       
+	        jsonResponseHeaders.add(HttpHeaders.TRANSFER_ENCODING, "binary");
+	        jsonResponseHeaders.set("Content-ID", "<root>");
+	        
+	        files = null;
+	        String jsonResponse = "{"
+	        		+ "    \"idelement\": {"
+	        		+ "        \"idtype\": 2,"
+	        		+ "        \"id\": \"2022ER0008851\""
+	        		+ "    },"
+	        		+ "    \"message\": \"Los cambios se realizaron\","
+	        		+ "    \"actionDate\": \"20240611063034-0500\""
+	        		+ "}";
+	        
 	        
 	        HttpEntity<String> jsonEntity = new HttpEntity<String>(jsonResponse, jsonResponseHeaders);
 	        HttpHeaders jsonFileHeaders = new HttpHeaders();
@@ -77,6 +134,97 @@ public class GesdocController {
 	        responseBody.clear();
 	        responseBody.set("root", jsonEntity);
 	        responseBody.set("etiqueta.png", fileEntity);
+	        
+	        return new ResponseEntity<MultiValueMap<String, Object>>(responseBody, headers, HttpStatus.OK);
+    	}catch(Exception ex) {
+    		ex.printStackTrace();
+    		return new ResponseEntity<String>("EXCEPCION ", HttpStatus.INTERNAL_SERVER_ERROR);
+    	}
+    }
+    
+    
+    
+    @PostMapping(value = "/mock/createSended", produces = {"multipart/mixed"}, consumes = {"multipart/form-data"})
+    public ResponseEntity<?> createSended(@RequestParam Map<String, MultipartFile> files)  throws Exception {
+    	
+    	try {
+	    	String fileName = "etiqueta.png";
+	        byte[] etiquetaBytes = readImageFromClasspath(fileName);
+	        System.out.println("etiquetaBytes = " + etiquetaBytes.length);
+	    	if (etiquetaBytes != null && etiquetaBytes.length <= 0) {
+	        	System.out.println("Archivo no existe");
+	            return ResponseEntity.notFound().build();
+	        }
+	        
+	        ByteArrayResource resource = new ByteArrayResource(etiquetaBytes) {
+	            @Override
+	            public String getFilename() {
+	                return fileName;
+	            }
+	        };
+	        
+	        HttpHeaders jsonResponseHeaders = new HttpHeaders();
+	        jsonResponseHeaders.setContentType(MediaType.APPLICATION_JSON_UTF8);       
+	        jsonResponseHeaders.add(HttpHeaders.TRANSFER_ENCODING, "binary");
+	        jsonResponseHeaders.set("Content-ID", "<root>");
+	        
+	        files = null;
+	        String jsonResponse = "{"
+	        		+ "    \"idelement\": {"
+	        		+ "        \"idtype\": 2,"
+	        		+ "        \"id\": \"2022ER0008851\""
+	        		+ "    },"
+	        		+ "    \"message\": \"Los cambios se realizaron\","
+	        		+ "    \"actionDate\": \"20240611063034-0500\""
+	        		+ "}";
+	        
+	        HttpEntity<String> jsonEntity = new HttpEntity<String>(jsonResponse, jsonResponseHeaders);
+	        HttpHeaders jsonFileHeaders = new HttpHeaders();
+	        jsonFileHeaders.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+	        jsonFileHeaders.add(HttpHeaders.TRANSFER_ENCODING, "binary");
+	        jsonFileHeaders.set("Content-ID", "<etiqueta.png>");
+	        HttpEntity<ByteArrayResource> fileEntity = new HttpEntity<ByteArrayResource>(resource, jsonFileHeaders);
+	        
+	        HttpHeaders headers = new HttpHeaders();
+	        headers.setContentType(MediaType.MULTIPART_MIXED);
+	        
+	        MultiValueMap<String, Object> responseBody = new LinkedMultiValueMap<>();
+	        responseBody.clear();
+	        responseBody.set("root", jsonEntity);
+	        responseBody.set("etiqueta.png", fileEntity);
+	        
+	        return new ResponseEntity<MultiValueMap<String, Object>>(responseBody, headers, HttpStatus.OK);
+    	}catch(Exception ex) {
+    		ex.printStackTrace();
+    		return new ResponseEntity<String>("EXCEPCION ", HttpStatus.INTERNAL_SERVER_ERROR);
+    	}
+    }
+    
+    
+    @PostMapping(value = "/mock/createRegistered", produces = {"multipart/mixed"}, consumes = {"multipart/form-data"})
+    public ResponseEntity<?> createRegistered(@RequestParam Map<String, MultipartFile> files)  throws Exception {
+    	
+    	try {	        
+	        HttpHeaders jsonResponseHeaders = new HttpHeaders();
+	        jsonResponseHeaders.setContentType(MediaType.APPLICATION_JSON_UTF8);       
+	        jsonResponseHeaders.add(HttpHeaders.TRANSFER_ENCODING, "binary");
+	        jsonResponseHeaders.set("Content-ID", "<root>");
+	        
+	        files = null;
+	        String jsonResponse = "{"
+	        		+ "    \"idelement\": {"
+	        		+ "        \"idtype\": 2,"
+	        		+ "        \"id\": \"2022ER0008851\""
+	        		+ "    },"
+	        		+ "    \"message\": \"Los cambios se realizaron\","
+	        		+ "    \"actionDate\": \"20240611063034-0500\""
+	        		+ "}";
+	        
+	        HttpEntity<String> jsonEntity = new HttpEntity<String>(jsonResponse, jsonResponseHeaders);
+	        HttpHeaders headers = new HttpHeaders();
+	        headers.setContentType(MediaType.MULTIPART_MIXED);
+	        MultiValueMap<String, Object> responseBody = new LinkedMultiValueMap<>();
+	        responseBody.set("root", jsonEntity);
 	        
 	        return new ResponseEntity<MultiValueMap<String, Object>>(responseBody, headers, HttpStatus.OK);
     	}catch(Exception ex) {
